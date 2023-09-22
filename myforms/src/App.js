0c9e44ee1a646296005363b0a6567e8b876ba315
NicoleMurillo <n.murillof@uniandes.edu.co>
2023-09-22 09:14:04 -0500

    cambios

--- myforms/src/App.js
+++ myforms/src/App.js
@@ -17,8 +17,25 @@ function App() {
   });
  
   const handlePasswordChange = ((e) => {
+    const passwordValue = e.target.value;
     setFormValues({...formValues, password: e.target.value})
+
+    const isValidPassword = /^(?=.*[A-Za-z])(?=.*\d).{9,}$/.test(passwordValue);
+    setValidationStates({...validationStates, passwordState: isValidPassword});
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const isValidEmail = /@gmail\.com$/.test(formValues.email);
+    setValidationStates({...validationStates, emailState: isValidEmail});
+
+    if (isValidEmail && validationStates.passwordState){
+      alert("Valid form.");
+    } else {
+      alert("Your email should follow an established format.");
+    }
+  }
  
   const handleSelectChange = ((e) => {
     setFormValues({...formValues, favClass: e.target.value})
@@ -32,30 +49,48 @@ function App() {
   return (
     <div>
       <h1>Ejemplo de formularios!</h1>
-     
-      <Form>
-      <Form.Group className="mb-6" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} value={formValues.email}/>
-        { !validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
-      </Form.Group>
- 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} value={formValues.password} />
-        { !validationStates.passwordState && <Form.Text className="text-muted">Your password should be have numbers and letters and should be at least 9 char long</Form.Text>}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Label>Favorite Class</Form.Label>
-        <Form.Select onChange={handleSelectChange}>
-          <option value="1">ISIS3710</option>
-          <option value="2">Programación con tecnologias web</option>
-        </Form.Select>
-      </Form.Group>
-      <Button variant="primary">
-        Submit
-      </Button>
-    </Form>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-6" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={handleEmailChange}
+          value={formValues.email}
+          />
+          {!validationStates.emailState && formValues.email && (
+            <Form.Text className="text.muted">
+              El correo debe tener un formato válido.
+            </Form.Text>
+          )}
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+            value={formValues.password}
+          />
+          {!validationStates.passwordState && formValues.password && (
+            <Form.Text className="text-muted">
+              La contraseña debe tener números, letras y ser al menos 9 caracteres de largo.
+            </Form.Text>
+          )}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Label>Favorite Class</Form.Label>
+          <Form.Select onChange={(e) => handleSelectChange(e)}>
+            <option value="1">ISIS3710</option>
+            <option value="2">Programación con tecnologías web</option>
+          </Form.Select>
+        </Form.Group>
+        <Button variant="primary" onClick={clickSubmit}>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
